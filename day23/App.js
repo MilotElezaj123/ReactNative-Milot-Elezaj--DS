d3531c051741b937e9807b246cfbd922928a2d2a
MilotElezaj123 <131296427+MilotElezaj123@users.noreply.github.com>
2025-05-14 20:42:12 +0200

    day23

---
+++ day23/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import{NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from './screen/Login';
+import Welcome from './screen/WelcomeScreen'
+
+export default function App() {
+  return (
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login}/>
+      <Stack.Screen name="Welcome" component={Welcome}/>
+    </Stack.Navigator>
+   </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ day23/screen/Login.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { View,Text,TextInput,Button,StyleSheet } from "react-native";
+
+const Login=()=>{
+    const handleLogin = async () => {
+    try{
+    const response = await fetch('http://dummyjson.com/auth/login',{
+       method:'POST',
+       header: {'Content-Type': 'application/json'},
+       body: JSON.stringify({
+        username: username,
+        password: password
+       }),
+       credentials: 'omit'
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if(data?.token){
+        navigator.navigate('Welcome',{
+            username: username
+        });
+    }else{
+        alert(data.message || 'Login failed. Please check your credentials');
+    }
+ }catch (err){
+     console.log('Login error: ',err);
+     alert('An error occured while logging in.');
+ }
+
+  };
+  return(
+    <View style={styles.container}>
+        <Text style={styles.title}>Fake API Login</Text>
+        <TextInput 
+         style={styles.input}
+         placeholder="Username"
+         autoCapitalize="none"
+         value={username}
+         onChangeText={test=>setUsername(test)}
+        />
+        <TextInput 
+         style={styles.input}
+         placeholder="Password"
+         autoCapitalize="none"
+         value={password}
+         onChangeText={test=>setPassword(test)}
+        />
+        <Button title="Login" onPress={handle}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: '#fff',
+    },
+    title:{
+        fontSize: 24,
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#aaa',
+        padding: 10,
+        marginVertical: 10,
+        borderRadius: 5,
+    },
+    error:{
+        color: 'red',
+        marginBottom: 10,
+        textAlign: 'center'
+    },
+})
+export default Login;
